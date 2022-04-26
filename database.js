7e16e391a4f05a00d2f0b258726e02764edcddd2
sauryaacharya <saurya.acharya9@gmail.com>
2022-04-25 21:02:57 -0400

    just kidding i forgot the constant

--- database.js
+++ database.js
@@ -17,10 +17,19 @@ if (row === undefined) {
 // Echo information about what you are doing to the console.
     console.log('Your database appears to be empty. I will initialize it now.');
 // Set a const that will contain your SQL commands to initialize the database.
-    const sqlInit = `
-        CREATE TABLE userinfo ( id INTEGER PRIMARY KEY, username TEXT, password TEXT );
-        INSERT INTO userinfo (username, password) VALUES ('user1','supersecurepassword'),('test','anotherpassword');
-    `;
+const sqlInit = `
+CREATE TABLE accesslog ( id INTEGER PRIMARY KEY, 
+    remoteaddr TEXT, 
+    remoteuser TEXT, 
+    time TEXT, 
+    method TEXT, 
+    url TEXT,  
+    protocol TEXT,
+    httpversion TEXT,
+    secure TEXT, 
+    status TEXT, 
+    referer TEXT, 
+    useragent TEXT);`;
 // Execute SQL commands that we just wrote above.
     db.exec(sqlInit);
 // Echo information about what we just did to the console.
